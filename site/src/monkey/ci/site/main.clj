5c83bf59a472dd48f638d8334fd7d8eccc71c5a0
Wout Neirynck <wout@monkey-projects.be>
2024-06-03 13:54:31 +0200

    Code example 2

--- site/src/monkey/ci/site/main.clj
+++ site/src/monkey/ci/site/main.clj
@@ -133,7 +133,7 @@
              "Simulate builds locally or in unit tests and avoid problems when deploying your app.")]])
 
 (def mockups
-  [:div.bg-soft-primary-light.pb-4
+  [:div.bg-soft-primary-light {:style {:padding-bottom "20em"}}
    [:div.container.content-space-t-1.content-space-t-md-3
     [:div.w-lg-65.text-center.mx-lg-auto.mb-7
      [:h3 "Built by Developers for Developers"]
@@ -175,6 +175,76 @@
      [:path {:d "M0 500H3000V0L0 500Z"
              :fill "#fff"}]]]])
 
+(def feature-1
+  [:div.d-flex
+   [:div.flex-shrink-0
+    [:span.svg-icon.svg-icon-sm.text-primary
+     [:svg {:width 24 :height 24 :viewbox "0 0 24 24" :fill "none" :xmlns "http://www.w3.org/2000/svg"}
+      [:path {:opacity "0.3"
+              :d "M7 20.5L2 17.6V11.8L7 8.90002L12 11.8V17.6L7 20.5ZM21 20.8V18.5L19 17.3L17 18.5V20.8L19 22L21 20.8Z"
+              :fill "#035A4B"}]
+      [:path {:d "M22 14.1V6L15 2L8 6V14.1L15 18.2L22 14.1Z"
+              :fill "#035A4B"}]]]]
+   [:div.flex-grow-1.ms-4
+    [:h5 "Use libraries from any Maven repository"]
+    [:p
+     "Publish your own libraries and use them in your scripts, or use already existing libs "
+     "to kick-start your builds."]]])
+
+(def feature-2
+  [:div.d-flex
+   [:div.flex-shrink-0
+    [:span.svg-icon.svg-icon-sm.text-primary
+     [:svg {:width 24 :height 24 :viewbox "0 0 24 24" :fill "none" :xmlns "http://www.w3.org/2000/svg"}
+      [:path {:fill-rule "evenodd"
+              :clip-rule "evenodd"
+              :d "M15 19.5229C15 20.265 15.9624 20.5564 16.374 19.9389L22.2227 11.166C22.5549 10.6676 22.1976 10 21.5986 10H17V4.47708C17 3.73503 16.0376 3.44363 15.626 4.06106L9.77735 12.834C9.44507 13.3324 9.80237 14 10.4014 14H15V19.5229Z"
+              :fill "#035A4B"}]
+      [:path {:opacity "0.3"
+              :fill-rule "evenodd"
+              :clip-rule "evenodd"
+              :d "M3 6.5C3 5.67157 3.67157 5 4.5 5H9.5C10.3284 5 11 5.67157 11 6.5C11 7.32843 10.3284 8 9.5 8H4.5C3.67157 8 3 7.32843 3 6.5ZM3 18.5C3 17.6716 3.67157 17 4.5 17H9.5C10.3284 17 11 17.6716 11 18.5C11 19.3284 10.3284 20 9.5 20H4.5C3.67157 20 3 19.3284 3 18.5ZM2.5 11C1.67157 11 1 11.6716 1 12.5C1 13.3284 1.67157 14 2.5 14H6.5C7.32843 14 8 13.3284 8 12.5C8 11.6716 7.32843 11 6.5 11H2.5Z"
+              :fill "#035A4B"}]]]]
+   [:div.flex-grow-1.ms-4
+    [:h5 "Zero Impact"]
+    [:p
+     "By only provisioning infrastructure as needed and using low-emission datacenters, we aim for a "
+     "zero-impact experience.  Any impact we do make is offset by donating to carbon-compensating "
+     "organizations."]]])
+
+(def code-fragment-2
+  (letfn [(p [s]
+            [:span.text-primary s])
+          (c [s]
+            [:span.text-muted s])
+          (k [s]
+            [:span.text-danger s])]
+    (u/code-editor
+     {:class "overflow-hidden"}
+     [[:span "(" (k "require") " '[monkey.ci.plugin.clj " (k ":as") " clj]))"]
+      ""
+      (c ";; Contains both test and publish jobs")
+      "(clj/deps-library)"])))
+
+(def features-expanded
+  [:div.overflow-hidden
+   [:div.container.content-space-1.content-space-b-md-3
+    [:div.row.align-items-lg-center
+     [:div.col-lg-6.mb-5.mb-lg-0
+      [:div.pe-lg-5
+       [:div.mb-7
+        [:h3 "Compact build scripts"]
+        [:p.fs-6
+         "Get rid of huge unmaintainable build scripts by using common libraries to make your scripts more compact. "
+         [:b "MonkeyCI"] " provides the means to easily achieve this using existing and battle-tested systems."]]
+       [:div.d-grid.gap-3
+        feature-1
+        feature-2]]]
+     [:div.col-lg-6
+      [:div.tab-content.mb-7
+       [:div.tab-pane.fade.show.active
+        code-fragment-2]]]]]])
+
 (def content
   "Main page content"
   [:main#content {:role "main"}
@@ -195,8 +265,8 @@
    [:div.border-top.mx-auto {:style "max-width: 25rem;"}]
    features
    mockups
-   ;; TODO More stuff to add here
-   #_shape-2
+   shape-2
+   features-expanded
    call-to-action])
 
 (def copyright (h/raw "&#169;"))

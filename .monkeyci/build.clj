1563763c9e0beb0f5cfa77d06665c49d562b4823
Wout Neirynck <wout@monkey-projects.be>
2024-11-20 10:15:52 +0100

    Build script job dependencies

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -28,6 +28,9 @@
   "True if any of the files in the `common/` dir have changed"
   (touched? #"^common/.*"))
 
+(def common-published?
+  (every-pred common-changed? bc/main-branch?))
+
 (defn- clj-cmd
   "Runs a local clojure command, without starting a container"
   [job-id cache-id cmd & [opts]]
@@ -40,26 +43,36 @@
 
 (defn run-tests
   "Runs tests for given site"
-  [id & [alias]]
-  (fn [_]
-    (-> (clj-cmd
-         (str "test-" id)
-         id
-         (format "-X%s:test:junit" (or (some-> alias str) "")))
-        (assoc :work-dir id))))
+  [id & [{:keys [alias] deps :dependencies}]]
+  (fn [ctx]
+    (letfn [(maybe-add-deps [job]
+              (let [d (deps ctx)]
+                (cond-> job
+                  d (assoc :dependencies d))))]
+      (-> (clj-cmd
+           (str "test-" id)
+           id
+           (format "-X%s:test:junit" (or (some-> alias str) "")))
+          (assoc :work-dir id)
+          (maybe-add-deps)))))
 
 (defn test-common [ctx]
   (when (common-changed? ctx)
     ((run-tests "common") ctx)))
-
-(def test-site (run-tests "site"))
-(def test-docs (run-tests "docs" :template))
 
 (defn deploy-common [ctx]
   (when (common-changed? ctx)
     (-> ((clj/deps-publish {:publish-job-id "deploy-common"
                             :test-job-id "test-common"}) ctx)
         (assoc :work-dir "common"))))
+
+(defn depends-on-common [ctx]
+  (when (common-published? ctx)
+    "deploy-common"))
+
+(def test-site (run-tests "site" {:dependencies depends-on-common}))
+(def test-docs (run-tests "docs" {:alias :template
+                                  :dependencies depends-on-common}))
 
 (defn build
   "Builds the website and docs files"
@@ -71,7 +84,7 @@
    {:save-artifacts [{:id id
                       :path (str id "/target")}]
     :dependencies (cond-> [(str "test-" id)]
-                    (common-changed? ctx) (conj "deploy-common"))
+                    (common-published? ctx) (conj "deploy-common"))
     :work-dir id}))
 
 (def build-site (partial build "site" :build))
@@ -84,7 +97,8 @@
    "-M:cryogen:build"
    {:work-dir "docs"
     :save-artifacts [{:id "docs-contents"
-                      :path "public"}]}))
+                      :path "public"}]
+    :dependencies ["build-docs"]}))
 
 (def img-base "fra.ocir.io/frjdhmocn5qi/website")
 

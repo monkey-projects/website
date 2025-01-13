fd6300e5a70a3d218147c9f926d0d615944a1d00
Wout Neirynck <wout@monkey-projects.be>
2025-01-13 13:55:12 +0100

    Split off cryogen repl code
    This to fix dependency issues when not including cryogen alias

--- docs/env/dev/user.clj
+++ docs/env/dev/user.clj
@@ -1,7 +1,5 @@
 (ns user
-  (:require [cryogen.server :as cs]
-            [cryogen-core.compiler :as ccc]
-            [monkey.ci.template.build :as tb]))
+  (:require [monkey.ci.template.build :as tb]))
 
 (defn build-docs
   "Builds the docs to target directory"
@@ -10,24 +8,3 @@
              :site-fn 'monkey.ci.docs.main/main
              :config {:base-url "staging.monkeyci.com"
                       :api-url "http://localhost:3000"}}))
-
-(defonce server (atom nil))
-
-(defn stop [s]
-  (when s
-    (.stop s)))
-
-(defn stop! []
-  (swap! server stop))
-
-(defn serve!
-  "Starts cryogen server for fast content editing"
-  []
-  (swap! server (fn [s]
-                  (stop s)
-                  (cs/serve {:join? false}))))
-
-(defn rebuild
-  "Performs a full build of the cryogen content."
-  []
-  (ccc/compile-assets-timed))

fc92b7ce446e1cd8e4d18157d58f033d35ffa236
Wout Neirynck <wout@monkey-projects.be>
2025-04-28 17:04:33 +0200

    Parse svg

--- common/src/monkey/ci/template/svg.clj
+++ common/src/monkey/ci/template/svg.clj
@@ -1,6 +1,14 @@
 (ns monkey.ci.template.svg
   "Functions to include svg images or icons in pages"
-  (:require [babashka.fs :as fs]))
+  (:require [babashka.fs :as fs]
+            [hickory.core :as h]))
 
-(defn include [conf n]
-  (slurp (fs/file (fs/path (:svg-path conf) (str n ".svg")))))
+(defn include
+  "Loads the svg file from the configured `svg-path` combining the name with
+   an `svg` extension.  The file is read and parsed into a hiccup structure."
+  [conf n]
+  (->> (fs/file (fs/path (:svg-path conf) (str n ".svg")))
+       (slurp)
+       (h/parse-fragment)
+       (map h/as-hiccup)
+       (first)))

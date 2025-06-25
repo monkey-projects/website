91da975510af2dbb73384b579b98b90bdaf1c9b6
Wout Neirynck <wout@monkey-projects.be>
2025-06-25 08:41:12 +0200

    Add default server config for root domain

--- run-podman.sh
+++ run-podman.sh
@@ -3,6 +3,7 @@
 # Run the nginx in a podman container
 podman run -it \
        -p 8083:8080 \
+       -p 8084:8081 \
        -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
        -v $PWD/docs/target/site:/var/www/html/docs \
        -v $PWD/site/target:/var/www/html/site \

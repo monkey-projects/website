68f35a59f007cdf3017d2d36aa1ca6485be6ea68
Wout Neirynck <wout@monkey-projects.be>
2026-02-10 09:11:22 +0100

    Title and author

--- site/src/monkey/ci/site/blog.clj
+++ site/src/monkey/ci/site/blog.clj
@@ -13,7 +13,7 @@
   (into (tc/head conf)
         [(tc/stylesheet (tc/assets-url conf "/css/github-dark.min.css"))]))
 
-(defn- apply-template [conf p]
+(defn- apply-template [entry conf]
   [:html
    (head (assoc conf :title "MonkeyCI - Blog"))
    (vec
@@ -21,7 +21,10 @@
      [:body
       (tc/header-light conf)
       [:div.container.content-space-1
-       [:div.w-lg-75.mx-lg-auto p]]]
+       [:div.w-lg-75.mx-lg-auto
+        [:h2.text-primary (:title entry)]
+        [:p.mb-4 [:em (:author entry) " - " (:date entry)]]
+        (:contents entry)]]]
      (t/footer conf)
      [(tc/script (tc/script-url conf "highlight.min.js"))
       (tc/script (tc/script-url conf "clojure.min.js"))
@@ -29,17 +32,24 @@
       [:script "hljs.highlightAll();"]]))])
 
 (defn generate-blog [conf f]
-  (-> (md/parse f)
-      (assoc :src f)
-      (update :contents (partial apply-template conf))))
+  (let [p (md/parse f)]
+    (assoc p
+           :src f
+           :contents (apply-template p conf))))
+
+(defn dest-file
+  "Generates destination file name for the blog entry, using the original name and date."
+  [{:keys [src date]}]
+  (fs/strip-ext (fs/file-name src)))
 
 (defn write-blog [dest entry]
-  (let [f (fs/path dest (fs/strip-ext (fs/file-name (:src entry))))]
+  (let [f (fs/path dest (dest-file entry))]
     (fs/create-dirs f)
     (assoc entry :dest (tb/generate f (constantly (:contents entry))))))
 
 (defn blog-pages [{{:keys [src dest]} :blog :as conf}]
   (->> (fs/list-dir src)
        (map (partial generate-blog conf))
+       (sort-by :date)
        (map (partial write-blog dest))
        (doall)))
--- site/test/monkey/ci/site/blog_test.clj
+++ site/test/monkey/ci/site/blog_test.clj
@@ -20,6 +20,25 @@
     (let [conf {:src (fs/path dir "src")
                 :dest (fs/path dir "dest")}]
       (is (nil? (spit (fs/file (fs/create-dirs (:src conf)) "test.md") "This is a test")))
-      (is (some? (sut/blog-pages {:blog conf})))
+      (not-empty (sut/blog-pages {:blog conf}))
+      
       (testing "generates directory per page"
-        (is (fs/exists? (:dest conf)))))))
+        (is (fs/exists? (:dest conf))))
+
+      (testing "generates archive page"))))
+
+(deftest generate-blog
+  (with-tmp-dir dir
+    (let [p (fs/path dir "test.md")]
+      (is (nil? (spit (fs/file p) (str (pr-str {:title "test article"} "\nThis is a test article")))))
+      (let [r (sut/generate-blog {} p)]
+        (testing "reads header from contents"
+          (is (= "test article" (:title r))))
+
+        (testing "adds contents"
+          (is (some? (:contents r))))))))
+
+(deftest dest-file
+  (testing "includes original file name without extension"
+    (is (= "test-entry"
+           (sut/dest-file {:src "/tmp/some/dir/test-entry.md"})))))
